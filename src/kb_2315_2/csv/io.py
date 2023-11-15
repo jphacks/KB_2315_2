886bf90d211dd46fbeafbd108998a3411bd56628
motty <motty.mio2@gmail.com>
2023-11-16 00:44:47 +0900

    return filename only

--- src/kb_2315_2/csv/io.py
+++ src/kb_2315_2/csv/io.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from pathlib import Path
 from uuid import uuid4
 
 import kb_2315_2.config as config
@@ -8,7 +7,7 @@ import kb_2315_2.config as config
 conf: config.env = config.read_config(dir=config.root_dir)
 
 
-def create_csv() -> Path:
+def create_csv() -> str:
     (config.root_dir / "data").mkdir(exist_ok=True)
 
     filename: str = datetime.now().strftime("%Y%m%d-%H%M%S-") + str(uuid4())[:8]
@@ -16,4 +15,4 @@ def create_csv() -> Path:
     while (config.root_dir / "data" / f"{filename}.csv").exists():
         filename = datetime.now().strftime("%Y%m%d-%H%M%S-") + str(uuid4())[:8]
 
-    return config.root_dir / "data" / f"{filename}.csv"
+    return filename
