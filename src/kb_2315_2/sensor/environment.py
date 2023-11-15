cff29b2bafe3e60cf09453501a06df0182399f77
motty <motty.mio2@gmail.com>
2023-11-16 00:44:36 +0900

    reconstruct sensor module

--- src/kb_2315_2/sensor/environment.py
+++ src/kb_2315_2/sensor/environment.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 
 class envirionment_sensor(BaseModel):
+    name: str = "envirionment_sensor"
     temperature: float = 0.0
     humidity: float = 0.0
-    pressure: float | None = None
+    pressure: float = 0.0
